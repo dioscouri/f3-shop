09014dad3ca82c855ce2bb7706187d2a9856fe0a
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2013-12-18 11:16:05 -0500

    added draft sample of how to add a menu-item option to your app's listener, refs dioscouri/f3-admin#2

--- src/Tienda/Listener.php
+++ src/Tienda/Listener.php
@@ -21,6 +21,22 @@ class Listener extends \Prefab
             
             \Dsc\System::instance()->addMessage('Tienda added its admin menu items.');
         }
+    }
+    
+    public function onAdminNavigationGetQuickAddItems( $event )
+    {
+        $items = $event->getArgument('items');
+    
+        $item = new \stdClass;
+        $item->title = 'Product Category';
+        $item->form = 'This would be a QUICK ADD form for adding a Product Category menu item.';
+        $items[] = $item;
         
+        $item = new \stdClass;
+        $item->title = 'Product Detail';
+        $item->form = 'This would be a QUICK ADD form for adding a Product Detail Page menu item.';
+        $items[] = $item;
+    
+        $event->setArgument('items', $items);
     }
 }
