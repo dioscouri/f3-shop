ce821871ab00a7c73dd30d07cc514b7177eaeec9
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-03-06 14:12:27 -0500

    actually forgot to save merged file

--- src/Shop/Admin/Controllers/MenuItemQuickAdd.php
+++ src/Shop/Admin/Controllers/MenuItemQuickAdd.php
@@ -3,37 +3,21 @@ namespace Shop\Admin\Controllers;
 
 class MenuItemQuickAdd extends \Admin\Controllers\BaseAuth 
 {
-	public function category($event)
+	public function collection($event)
 	{
-<<<<<<< HEAD
-		$model = \Shop\Models\Collections::instance();
-=======
-		$model = new \Shop\Models\Categories;
->>>>>>> 519123d5d7783f66d0ff05e7fd112f2e1ef14dd5
-		$categories = $model->getList();
-		\Base::instance()->set('collections', $collections );
-		
 		$view = \Dsc\System::instance()->get('theme');
 		return $view->renderLayout('Shop/Admin/Views::quickadd/collection.php');
 	}
-	
+
 	public function category($event)
 	{
-	    $model = \Shop\Models\Categories::instance();
-	    $categories = $model->getList();
-	    \Base::instance()->set('categories', $categories );
-	
 	    $view = \Dsc\System::instance()->get('theme');
 	    return $view->renderLayout('Shop/Admin/Views::quickadd/category.php');
 	}
 	
 	public function product($event)
 	{
-<<<<<<< HEAD
-	    $model = \Shop\Models\Products::instance();
-=======
 	    $model = new \Shop\Models\Products;
->>>>>>> 519123d5d7783f66d0ff05e7fd112f2e1ef14dd5
 	    $tags = $model->getTags();
 	    \Base::instance()->set('tags', $tags );
 	
