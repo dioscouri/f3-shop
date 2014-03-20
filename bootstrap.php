4802f943d6ed016eab73228b480a450db888ee88
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-03-20 14:14:07 -0400

    minor formatting change

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 <?php
-class ShopBootstrap extends \Dsc\BaseBootstrap
+class ShopBootstrap extends \Dsc\Bootstrap
 {
     protected $dir = __DIR__;
     protected $namespace = 'Shop';
-=======
-<?php 
-class ShopBootstrap extends \Dsc\Bootstrap{
-	protected $dir = __DIR__;
-	protected $namespace = 'Shop';
->>>>>>> 2a7664e488a270437128579631b82859632a9607
 
     protected function runAdmin()
     {
@@ -24,23 +17,22 @@ class ShopBootstrap extends \Dsc\Bootstrap{
         }
         catch ( \Exception $e )
         {
-            
         }
     }
-    
+
     protected function preSite()
     {
         // add the css & js files to the minifier
-        \Minify\Factory::registerPath( $this->dir . "/src/");
+        \Minify\Factory::registerPath( $this->dir . "/src/" );
         
         $files = array(
-            'Shop/Assets/js/site.js'
+            'Shop/Assets/js/site.js' 
         );
         
-        foreach ($files as $file)
+        foreach ( $files as $file )
         {
-            \Minify\Factory::js($file);
-        }        
+            \Minify\Factory::js( $file );
+        }
     }
 }
 
