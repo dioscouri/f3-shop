4f9e0ae0c6ffd28d9531e48ef1ea28bb1dd8aad6
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-19 15:31:43 -0400

    formatting change only

--- src/Shop/Models/Collections.php
+++ src/Shop/Models/Collections.php
@@ -1,31 +1,39 @@
-<?php 
+<?php
 namespace Shop\Models;
 
-class Collections extends \Dsc\Mongo\Collections\Describable 
+class Collections extends \Dsc\Mongo\Collections\Describable
 {
+
     public $products = array();
+
     public $categories = array();
+
     public $featured_image;
+
     public $publication_status = "published";
+
     public $inventory_status = "in_stock";
-    
+
     protected $__collection_name = 'shop.collections';
+
     protected $__type = 'shop.collections';
-    
+
     /**
      * For the provided collection_id, create the query params to be used in \Shop\Models\Products
      * Trigger plugin events so they can modify the conditions.
-     * 
-     * @param unknown $collection_id
+     *
+     * @param unknown $collection_id            
      */
-    public static function getProductQueryConditions( $collection_id )
+    public static function getProductQueryConditions($collection_id)
     {
-        $collection = (new static())->load(array('_id'=> new \MongoId( (string) $collection_id) ));         
-        if (empty($collection->id)) 
+        $collection = (new static())->load(array(
+            '_id' => new \MongoId((string) $collection_id)
+        ));
+        if (empty($collection->id))
         {
-        	return array();
+            return array();
         }
-
+        
         $conditions = array();
         
         if (!empty($collection->products))
@@ -33,47 +41,73 @@ class Collections extends \Dsc\Mongo\Collections\Describable
             $ids = array();
             foreach ($collection->products as $id)
             {
-                $ids[] = new \MongoId( (string) $id);
+                $ids[] = new \MongoId((string) $id);
             }
-             
-            $conditions = array_merge( $conditions, array( '_id' => array( '$in' => $ids ) ) );
+            
+            $conditions = array_merge($conditions, array(
+                '_id' => array(
+                    '$in' => $ids
+                )
+            ));
         }
         
         // a range search
         if (!empty($collection->price_minimum) && !empty($collection->price_maximum))
         {
             $and = array(
-                array( 'prices.default' => array( '$lte' => (float) $collection->price_maximum ) ),
-                array( 'prices.default' => array( '$gte' => (float) $collection->price_minimum ) ),
+                array(
+                    'prices.default' => array(
+                        '$lte' => (float) $collection->price_maximum
+                    )
+                ),
+                array(
+                    'prices.default' => array(
+                        '$gte' => (float) $collection->price_minimum
+                    )
+                )
             );
             
-            if (!empty($conditions['$and'])) {
-                $conditions['$and'] = array_merge( $conditions['$and'], $and );
-            } else {
+            if (!empty($conditions['$and']))
+            {
+                $conditions['$and'] = array_merge($conditions['$and'], $and);
+            }
+            else
+            {
                 $conditions['$and'] = $and;
             }
-            
         }
         // everything $gt a minimum
-        elseif (!empty($collection->price_minimum) && empty($collection->price_maximum)) 
+        elseif (!empty($collection->price_minimum) && empty($collection->price_maximum))
         {
-            $conditions = array_merge( $conditions, array( 'prices.default' => array('$gte' => (float) $collection->price_minimum) ) );
+            $conditions = array_merge($conditions, array(
+                'prices.default' => array(
+                    '$gte' => (float) $collection->price_minimum
+                )
+            ));
         }
         // everything $lt a maximum
         elseif (empty($collection->price_minimum) && !empty($collection->price_maximum))
         {
-            $conditions = array_merge( $conditions, array( 'prices.default' => array('$lte' => (float) $collection->price_maximum) ) );
+            $conditions = array_merge($conditions, array(
+                'prices.default' => array(
+                    '$lte' => (float) $collection->price_maximum
+                )
+            ));
         }
         
-        if (!empty($collection->categories)) 
+        if (!empty($collection->categories))
         {
             $cats = array();
-        	foreach ($collection->categories as $cat) 
-        	{
-        	    $cats[] = new \MongoId( (string) $cat['id']);
-        	}
-        	
-        	$conditions = array_merge( $conditions, array( 'categories.id' => array( '$in' => $cats ) ) );        	
+            foreach ($collection->categories as $cat)
+            {
+                $cats[] = new \MongoId((string) $cat['id']);
+            }
+            
+            $conditions = array_merge($conditions, array(
+                'categories.id' => array(
+                    '$in' => $cats
+                )
+            ));
         }
         
         if (!empty($collection->tags))
@@ -84,103 +118,145 @@ class Collections extends \Dsc\Mongo\Collections\Describable
                 $tags[] = $tag;
             }
             
-            if (!empty($conditions['tags'])) 
+            if (!empty($conditions['tags']))
             {
-                if (is_array($conditions['tags'])) {
-                    if (!empty($conditions['tags']['$in'])) {
-                    	foreach ($conditions['tags']['$in'] as $tag) {
-                    		if (!in_array($tag, $tags)) {
-                    			$tags[] = $tag;
-                    		}
-                    	}
+                if (is_array($conditions['tags']))
+                {
+                    if (!empty($conditions['tags']['$in']))
+                    {
+                        foreach ($conditions['tags']['$in'] as $tag)
+                        {
+                            if (!in_array($tag, $tags))
+                            {
+                                $tags[] = $tag;
+                            }
+                        }
                     }
-                } 
-                else {
-                	$tags[] = $conditions['tags'];
+                }
+                else
+                {
+                    $tags[] = $conditions['tags'];
                 }
                 
-                $conditions['tags'] = array( '$in' => $tags );
-                
-            } else {
-                $conditions['tags'] = array( '$in' => $tags );
-            }            
+                $conditions['tags'] = array(
+                    '$in' => $tags
+                );
+            }
+            else
+            {
+                $conditions['tags'] = array(
+                    '$in' => $tags
+                );
+            }
         }
         
-        if (!empty($collection->publication_status)) 
+        if (!empty($collection->publication_status))
         {
-            $conditions = array_merge( $conditions, array( 'publication.status' => $collection->publication_status ) );
-            if ($collection->publication_status == 'published') 
+            $conditions = array_merge($conditions, array(
+                'publication.status' => $collection->publication_status
+            ));
+            if ($collection->publication_status == 'published')
             {
                 $and = array(
-                                array('$or' => array(
-                                                array('publication.start.time' => null),
-                                                array('publication.start.time' => array( '$lte' => time() )  )
-                                )),
-                                array('$or' => array(
-                                                array('publication.end.time' => null),
-                                                array('publication.end.time' => array( '$gt' => time() )  )
-                                ))
+                    array(
+                        '$or' => array(
+                            array(
+                                'publication.start.time' => null
+                            ),
+                            array(
+                                'publication.start.time' => array(
+                                    '$lte' => time()
+                                )
+                            )
+                        )
+                    ),
+                    array(
+                        '$or' => array(
+                            array(
+                                'publication.end.time' => null
+                            ),
+                            array(
+                                'publication.end.time' => array(
+                                    '$gt' => time()
+                                )
+                            )
+                        )
+                    )
                 );
-                //$conditions = array_merge( $conditions, array( '$and' => $and ) );
-                if (!empty($conditions['$and'])) {
-                    $conditions['$and'] = array_merge( $conditions['$and'], $and );
-                } else {
+                // $conditions = array_merge( $conditions, array( '$and' => $and ) );
+                if (!empty($conditions['$and']))
+                {
+                    $conditions['$and'] = array_merge($conditions['$and'], $and);
+                }
+                else
+                {
                     $conditions['$and'] = $and;
-                }                
+                }
             }
         }
         
         if (!empty($collection->inventory_status))
         {
-            switch($collection->inventory_status) {
-            	case "low_stock":
-            	    $conditions = array_merge( $conditions, array( 'inventory_count' => array( '$lte' => 20 ) ) );
-            	    break;
-            	case "no_stock":
-            	    $conditions = array_merge( $conditions, array( 'inventory_count' => array( '$lte' => 0 ) ) );
-            	    break;
-            	case "in_stock":
-            	    $conditions = array_merge( $conditions, array( 'inventory_count' => array( '$gte' => 1 ) ) );
-            	    break;
-            }            
+            switch ($collection->inventory_status)
+            {
+                case "low_stock":
+                    $conditions = array_merge($conditions, array(
+                        'inventory_count' => array(
+                            '$lte' => 20
+                        )
+                    ));
+                    break;
+                case "no_stock":
+                    $conditions = array_merge($conditions, array(
+                        'inventory_count' => array(
+                            '$lte' => 0
+                        )
+                    ));
+                    break;
+                case "in_stock":
+                    $conditions = array_merge($conditions, array(
+                        'inventory_count' => array(
+                            '$gte' => 1
+                        )
+                    ));
+                    break;
+            }
         }
         
         // allow event listeners to modify the query conditions
         $eventName = "ShopModelsCollections_getProductQueryConditions";
-        $event = \Dsc\System::instance()->trigger( $eventName, array(
-                        'conditions' => $conditions,
-                        'collection' => $collection
-        ) );
+        $event = \Dsc\System::instance()->trigger($eventName, array(
+            'conditions' => $conditions,
+            'collection' => $collection
+        ));
         
         $conditions = $event->getArgument('conditions');
-
-        return $conditions;
         
+        return $conditions;
     }
-    
+
     /**
      * Gets a count of all products returned by this collection's query
      *
-     * @param string $id
+     * @param string $id            
      * @return multitype: multitype:string
      */
-    public static function productCount( $id = null )
+    public static function productCount($id = null)
     {
         $result = 0;
-        if (empty( $id ))
+        if (empty($id))
         {
             return $result;
         }
         
         $conditions = static::getProductQueryConditions($id);
-        $result = (new \Shop\Models\Products())->collection()->count( $conditions );
-    
+        $result = (new \Shop\Models\Products())->collection()->count($conditions);
+        
         return $result;
     }
-    
+
     protected function beforeValidate()
     {
-
         if (!empty($this->categories))
         {
             $category_ids = array();
@@ -189,20 +265,29 @@ class Collections extends \Dsc\Mongo\Collections\Describable
             $this->set('categories', array());
             
             // convert each into an array of values if they aren't already
-            foreach ($current as $category) 
+            foreach ($current as $category)
             {
-            	if (is_array($category)) {
-            		$categories[] = $category;
-            	} elseif (!empty($category)) {
-            		$category_ids[] = $category;
-            	}
-            }
-
-            if (!empty($category_ids)) 
-            {
-                if ($list = (new \Shop\Models\Categories)->setState('select.fields', array('title', 'slug'))->setState('filter.ids', $category_ids)->getList())
+                if (is_array($category))
                 {
-                    foreach ($list as $list_item) {
+                    $categories[] = $category;
+                }
+                elseif (!empty($category))
+                {
+                    $category_ids[] = $category;
+                }
+            }
+            
+            if (!empty($category_ids))
+            {
+                if ($list = (new \Shop\Models\Categories())->setState('select.fields', array(
+                    'title',
+                    'slug'
+                ))
+                    ->setState('filter.ids', $category_ids)
+                    ->getList())
+                {
+                    foreach ($list as $list_item)
+                    {
                         $cat = array(
                             'id' => $list_item->id,
                             'title' => $list_item->title,
@@ -212,10 +297,10 @@ class Collections extends \Dsc\Mongo\Collections\Describable
                     }
                 }
             }
-           
+            
             $this->set('categories', $categories);
         }
-
+        
         return parent::beforeValidate();
     }
 
@@ -225,21 +310,22 @@ class Collections extends \Dsc\Mongo\Collections\Describable
         
         return parent::beforeCreate();
     }
-    
+
     protected function beforeSave()
     {
         if (!empty($this->products) && !is_array($this->products))
         {
             $this->products = trim($this->products);
-            if (!empty($this->products)) {
-                $this->products = \Base::instance()->split( (string) $this->products );
+            if (!empty($this->products))
+            {
+                $this->products = \Base::instance()->split((string) $this->products);
             }
         }
-        elseif(empty($this->products) && !is_array($this->products))
+        elseif (empty($this->products) && !is_array($this->products))
         {
             $this->products = array();
         }
-
+        
         return parent::beforeSave();
     }
 }
