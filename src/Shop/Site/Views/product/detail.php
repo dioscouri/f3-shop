b37837f1b1d23495d1a3d999ea282817aa0a69ef
Lukas Polak <lpolak@dioscouri.com>
2014-06-10 01:31:32 -0400

    OK, let's synchronize the CSS class for price on related product with f3-blog

--- src/Shop/Site/Views/product/detail.php
+++ src/Shop/Site/Views/product/detail.php
@@ -267,7 +267,7 @@ jQuery(document).ready(function(){
                                 <span class="list-price"><strike><?php echo \Shop\Models\Currency::format( $product->{'prices.list'} ); ?></strike></span>
                             <?php } ?>
                             &nbsp;
-                            <div class="current-price">
+                            <div class="price">
                                 <?php echo \Shop\Models\Currency::format( $product->price() ); ?>
                             </div>
     
