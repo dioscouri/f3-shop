b37510819e6c7ed49c7a757c228d2641395ef964
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-09 20:01:04 -0400

    forgot to add a view file

---
+++ src/Shop/Admin/Views/campaigns/fields_expiration.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="col-md-2">
+        
+        <h3>Expiration</h3>
+                
+    </div>
+    <!-- /.col-md-2 -->
+                
+    <div class="col-md-10">
+    
+        <div class="form-group">
+            <label>Action to perform upon expiration #1</label>
+            <?php /*?><input type="text" name="title" placeholder="Title" value="<?php echo $flash->old('title'); ?>" class="form-control" /> */ ?>
+        </div>
+        <!-- /.form-group -->
+        
+    </div>
+    <!-- /.col-md-10 -->
+    
+</div>
+<!-- /.row -->
+
+<hr />
