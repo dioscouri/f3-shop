9b801d78dfec2565451789fffc02bc0eedffebc1
Building Berms Server <chris@ammonitenetworks.com>
2014-06-21 20:52:26 +0000

    going to tabs view for shipping

--- src/Shop/Admin/Views/settings/shipping.php
+++ src/Shop/Admin/Views/settings/shipping.php
@@ -1,61 +1,94 @@
-<h3 class="">Shipping Methods</h3>
-<hr />
+<script src="./ckeditor/ckeditor.js"></script>
+<script>
+jQuery(document).ready(function(){
+    CKEDITOR.replaceAll( 'wysiwyg' );    
+});
+</script>
 
-<div class="">
-	<div class="panel-group" id="accordion">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#ups">
-          UPS<span class="badge pull-right"><?php echo $flash->old('shipping.ups.mode');?></span>
-        </a>
-      </h4>
+<div class="well">
+
+<form id="settings-form" role="form" method="post" class="form-horizontal clearfix">
+
+    <div class="clearfix">
+        <button type="submit" class="btn btn-primary pull-right">Save Changes</button>
     </div>
-    <div id="ups" class="panel-collapse collapse in">
-      <div class="panel-body">
-	      <div class="container">
-	          <?php echo $this->renderLayout('Shop/Admin/Views::settings/shipping/ups.php'); ?>
-	      </div>
-     </div>
+    
+    <hr/>
+
+    <div class="row">
+        <div class="col-md-3 col-sm-4">
+            <ul class="nav nav-pills nav-stacked">
+                <li class="active">
+                    <a href="#tab-general" data-toggle="tab"> General Settings </a>
+                </li>            
+                <li>
+                    <a href="#tab-ups" data-toggle="tab"> UPS </a>
+                </li>
+                <li>
+                    <a href="#tab-fedex" data-toggle="tab"> FEDEX </a>
+                </li>
+                <li>
+                    <a href="#tab-usps" data-toggle="tab"> USPS </a>
+                </li>
+                <li>
+                    <a href="#tab-stamps" data-toggle="tab"> STAMPS </a>
+                </li>                
+                
+                                         
+                <?php if (!empty($this->event)) { foreach ((array) $this->event->getArgument('tabs') as $key => $title ) { ?>
+                <li>
+                    <a href="#tab-<?php echo $key; ?>" data-toggle="tab"> <?php echo $title; ?> </a>
+                </li>
+                <?php } } ?>                
+            </ul>
+        </div>
+
+        <div class="col-md-9 col-sm-8">
+
+            <div class="tab-content stacked-content">
+            
+                <div class="tab-pane fade in active" id="tab-general">
+                
+                    <?php echo $this->renderLayout('Shop/Admin/Views::settings/shipping/general.php'); ?>
+
+                </div>
+            
+                <div class="tab-pane fade in" id="tab-ups">
+                
+                    <?php echo $this->renderLayout('Shop/Admin/Views::settings/shipping/ups.php'); ?>
+
+                </div>
+                
+                <div class="tab-pane fade in" id="tab-fedex">
+                
+                    <?php echo $this->renderLayout('Shop/Admin/Views::settings/shipping/fedex.php'); ?>
+
+                </div>
+                
+                <div class="tab-pane fade in" id="tab-usps">
+                
+                    <?php echo $this->renderLayout('Shop/Admin/Views::settings/shipping/usps.php'); ?>
+
+                </div>
+                
+                <div class="tab-pane fade in" id="tab-stamps">
+                
+                    <?php echo $this->renderLayout('Shop/Admin/Views::settings/shipping/stamps.php'); ?>
+
+                </div>
+                
+               
+                <?php if (!empty($this->event)) { foreach ((array) $this->event->getArgument('content') as $key => $content ) { ?>
+                <div class="tab-pane fade in" id="tab-<?php echo $key; ?>">
+                    <?php echo $content; ?>
+                </div>
+                <?php } } ?>
+
+            </div>
+
+        </div>
     </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#usps">
-          USPS<span class="badge pull-right"><?php echo $flash->old('shipping.usps.mode');?></span>
-        </a>
-      </h4>
-    </div>
-    <div id="usps" class="panel-collapse collapse">
-      <div class="panel-body">
-	 <div class="container">
-		          <?php echo $this->renderLayout('Shop/Admin/Views::settings/shipping/usps.php'); ?>
-		      </div>
-		   </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#fedex">
-          FEDEX<span class="badge pull-right"><?php echo $flash->old('shipping.fedex.mode');?></span>
-        </a>
-      </h4>
-    </div>
-    <div id="fedex" class="panel-collapse collapse">
-      <div class="panel-body">
-		<div class="container">
-		          <?php echo $this->renderLayout('Shop/Admin/Views::settings/shipping/fedex.php'); ?>
-		</div>
-		      
-      </div>
-    </div>
-  </div>
+
+</form>
+
 </div>
-
-
-
-</div>
-<!-- /.form-group -->
-
