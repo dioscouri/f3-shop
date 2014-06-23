85e39df2897715510ff21f25a422b38fcbf2f702
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-23 15:32:28 -0400

    made wishlist view responsive, fixes dioscouri/amritasingh-f3#222

--- src/Shop/Site/Views/wishlist/read.php
+++ src/Shop/Site/Views/wishlist/read.php
@@ -43,73 +43,57 @@
 
 <?php if (!empty($wishlist->items)) { ?>
 
-    <div class="row">
-        <div class="col-sm-12">
-            <form method="post">
-            <div class="table-responsive shopping-cart">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th width="41%"><div class="title-wrap">Product</div></th>
-                        <th width="14%"><div class="title-wrap">Unit Price</div></th>
-                        <th width="14%"><div class="title-wrap">Status</div></th>
-                        <th width="3%"><div class="title-wrap"><i class="glyphicon glyphicon-remove"></i></div></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    
-                    <?php foreach ($wishlist->items as $key=>$item) { ?>
-                    <tr>
-                        <td>
-                            <div class="cart-product">
-                                <figure>
-                                    <?php if (\Dsc\ArrayHelper::get($item, 'image')) { ?>
-                                    <a href="./shop/product/<?php echo \Dsc\ArrayHelper::get($item, 'product.slug'); ?>">
-                                        <img src="./asset/thumb/<?php echo \Dsc\ArrayHelper::get($item, 'image'); ?>" alt=""/>
-                                    </a>
-                                    <?php } ?>
-                                </figure>
-                                <div class="text">
-                                    <h2>
-                                        <a href="./shop/product/<?php echo \Dsc\ArrayHelper::get($item, 'product.slug'); ?>"><?php echo \Dsc\ArrayHelper::get($item, 'product.title'); ?></a>
-                                        <?php if (\Dsc\ArrayHelper::get($item, 'attribute_title')) { ?>
-                                        <div><small><?php echo \Dsc\ArrayHelper::get($item, 'attribute_title'); ?></small></div>
-                                        <?php } ?>                                            
-                                    </h2>
-                                    <div class="details">
-                                        <?php if (\Dsc\ArrayHelper::get($item, 'sku')) { ?>
-                                        <span class="detail-line">
-                                            <strong>SKU:</strong> <?php echo \Dsc\ArrayHelper::get($item, 'sku'); ?>
-                                        </span>
-                                        <?php } ?>
-                                        
-                                    </div>
+    <?php foreach ($wishlist->items as $key=>$item) { ?>
+        <div class="list-group-item">
+            <div class="row">
+                <div class="col-xs-6 col-sm-8 col-md-8">
+                    <div class="row">
+                        <div class="col-xs-4 col-sm-3 col-md-3">
+                            <figure>
+                                <?php if (\Dsc\ArrayHelper::get($item, 'image')) { ?>
+                                <a href="./shop/product/<?php echo \Dsc\ArrayHelper::get($item, 'product.slug'); ?>">
+                                    <img class="img-responsive" src="./asset/thumb/<?php echo \Dsc\ArrayHelper::get($item, 'image'); ?>" alt=""/>
+                                </a>
+                                <?php } ?>
+                            </figure>                
+                        </div>
+                        <div class="col-xs-8 col-sm-9 col-md-9">
+                            <div class="text">
+                                <h4>
+                                    <a href="./shop/product/<?php echo \Dsc\ArrayHelper::get($item, 'product.slug'); ?>"><?php echo \Dsc\ArrayHelper::get($item, 'product.title'); ?></a>
+                                    <?php if (\Dsc\ArrayHelper::get($item, 'attribute_title')) { ?>
+                                    <div><small><?php echo \Dsc\ArrayHelper::get($item, 'attribute_title'); ?></small></div>
+                                    <?php } ?>                                            
+                                </h4>
+                                <div class="details">
+                                    <?php if (\Dsc\ArrayHelper::get($item, 'sku')) { ?>
+                                    <span class="detail-line">
+                                        <strong>SKU:</strong> <?php echo \Dsc\ArrayHelper::get($item, 'sku'); ?>
+                                    </span>
+                                    <?php } ?>                            
                                 </div>
-                            </div>
-                            
-                        </td>
-                        <td><div class="price"><?php echo \Shop\Models\Currency::format( \Dsc\ArrayHelper::get($item, 'price') ); ?></div></td>
-
-                        <td>
-                            <div class="price text-center">
-                            <?php if (\Shop\Models\Variants::quantity(\Dsc\ArrayHelper::get($item, 'variant_id'))) { ?>
-                                <a class="btn btn-default" data-product-sku="<?php echo  \Dsc\ArrayHelper::get($item, 'sku'); ?>" data-product-variant="<?php echo \Dsc\ArrayHelper::get($item, 'attribute_title'); ?>" data-product-name="<?php echo \Dsc\ArrayHelper::get($item, 'product.title'); ?>" href="./shop/wishlist/<?php echo $wishlist->id; ?>/cart/<?php echo \Dsc\ArrayHelper::get($item, 'hash'); ?>" data-button="add-to-cart">Add to Cart</a>
-                            <?php } else { ?>
-                                Unavailable
-                            <?php } ?>
-                            </div>
-                        </td>
-                        <td><a href="./shop/wishlist/remove/<?php echo \Dsc\ArrayHelper::get($item, 'hash'); ?>" class="btn btn-default custom-button"><i class="glyphicon glyphicon-remove"></i></a></td>
-                    </tr>
-                    <?php } ?>
-                    
-                    </tbody>
-                </table>
-            </div>
+                                <div class="price"><?php echo \Shop\Models\Currency::format( \Dsc\ArrayHelper::get($item, 'price') ); ?></div>
+                            </div>                
+                        </div>
+                    </div>
+                </div>
             
-            </form>                
+                <div class="col-xs-4 col-sm-3 col-md-3">
+                    <div class="price text-center">
+                    <?php if (\Shop\Models\Variants::quantity(\Dsc\ArrayHelper::get($item, 'variant_id'))) { ?>
+                        <a class="btn btn-default" data-product-sku="<?php echo  \Dsc\ArrayHelper::get($item, 'sku'); ?>" data-product-variant="<?php echo \Dsc\ArrayHelper::get($item, 'attribute_title'); ?>" data-product-name="<?php echo \Dsc\ArrayHelper::get($item, 'product.title'); ?>" href="./shop/wishlist/<?php echo $wishlist->id; ?>/cart/<?php echo \Dsc\ArrayHelper::get($item, 'hash'); ?>" data-button="add-to-cart">Add to Cart</a>
+                    <?php } else { ?>
+                        Unavailable
+                    <?php } ?>
+                    </div>
+                </div>
+                
+                <div class="col-xs-2 col-sm-1 col-md-1">
+                    <a href="./shop/wishlist/remove/<?php echo \Dsc\ArrayHelper::get($item, 'hash'); ?>" class="btn btn-default custom-button"><i class="glyphicon glyphicon-remove"></i></a>
+                </div>
+            </div>
         </div>
-    </div>
+    <?php } ?>
     
 <?php } ?>
 
