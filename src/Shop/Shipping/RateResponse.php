d445a483dd4f37ce6f70839882a1a19561ceca6a
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-07-26 09:33:28 -0400

    formatting in shipment classes and fixed class name

--- src/Shop/Shipping/RateResponse.php
+++ src/Shop/Shipping/RateResponse.php
@@ -1,24 +1,22 @@
 <?php
 /**
  * The package class creates an object to hold the response from a rate query.
- * 
-
- * 
  */
 namespace Shop\Shipping;
 
-class RateResponse {
-    
+class RateResponse
+{
+
     /**
      *
      * @var string of rate request status - can be 'Success' or 'Error'
      */
     public $status = null;
-    
+
     /**
      * Holds the details of each shipping service available
-     * @var array
-     *  Each array element will contain:
+     * 
+     * @var array Each array element will contain:
      *      array [messages] an array of any messages associated with this service
      *      string [service_code] the shippers service code for the method of transit
      *      string [service_description] the description of the method of transit
@@ -26,26 +24,26 @@ class RateResponse {
      *      string [currency] the currency method used
      *      integer [package_count] the number of packages in the shipment
      *      array [packages] an array holding packages data
-     *          each array element will contain:
-     *          float [base_cost] the base cost to ship this package
-     *          float [option_cost] the cost for any options on this package
-     *          float [total_cost] the total cost to ship this package
-     *          float|integer [weight] the weight of this package
-     *          float|integer [billed_weight] the billed weight of this package
-     *          string [weight_unit] the unit of measure used for weight 'LB' or 'KG'
+     *      each array element will contain:
+     *      float [base_cost] the base cost to ship this package
+     *      float [option_cost] the cost for any options on this package
+     *      float [total_cost] the total cost to ship this package
+     *      float|integer [weight] the weight of this package
+     *      float|integer [billed_weight] the billed weight of this package
+     *      string [weight_unit] the unit of measure used for weight 'LB' or 'KG'
      */
     public $services = array();
 
-    
     /**
-     * 
-     * @param status $status the status of the request - 'Success' or 'Error'
+     *
+     * @param status $status
+     *            the status of the request - 'Success' or 'Error'
      * @version updated 12/28/2012
      * @since 12/08/2012
      */
-    public function __construct($status) {
+    public function __construct($status)
+    {
         // set class properties
         $this->status = $status;
     }
-    
 }
--- src/Shop/Shipping/ShipmentInterface.php
+++ src/Shop/Shipping/ShipmentInterface.php
@@ -1,15 +1,19 @@
 <?php
 /**
  * The shipper interface is for all shipping vendor classes.
- * 
- * 
  */
 namespace Shop\Shipping;
 
-interface ShipperInterface {
-    public function setShipment(Shipment $Shipment);
+interface ShipmentInterface
+{
+
+    public function setShipment(\Shop\Shipping\Shipment $Shipment);
+
     public function setConfig(array $config);
+
     public function getRate();
+
     public function createLabel();
+
     public function validForCart();
 }
