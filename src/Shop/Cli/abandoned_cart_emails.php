e091ecbb1f674f147ae283f3cf2fb290f760d38b
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-07-29 13:07:37 -0400

    added CLI placeholder for abandoned cart emails, refs dioscouri/amritasingh-f3#375

---
+++ src/Shop/Cli/abandoned_cart_emails.php
@@ -0,0 +1,16 @@
+<?php
+define('PATH_ROOT', realpath( __dir__ . '/../../../../../../' ) . '/' );
+$app = require( PATH_ROOT . 'vendor/bcosca/fatfree/lib/base.php');
+$app->set('PATH_ROOT', PATH_ROOT);
+$app->set('AUTOLOAD', $app->get('PATH_ROOT') . 'apps/;');
+
+require $app->get('PATH_ROOT') . 'vendor/autoload.php';
+
+$app->set('APP_NAME', 'cli');
+
+require $app->get('PATH_ROOT') . 'config/config.php';
+
+//add a route
+$app->route('GET /', '\Shop\Models\CartsAbandoned->findAndSendEmailsAccordingToRules');
+
+$app->run();
