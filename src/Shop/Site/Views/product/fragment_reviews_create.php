62415817e36e2fb9d1f87194d8f546e45bea0f4c
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-09-17 15:59:02 -0400

    adding review form

---
+++ src/Shop/Site/Views/product/fragment_reviews_create.php
@@ -0,0 +1,18 @@
+<div class="reviews-form">
+    <div id="create-review-panel" class="panel-group">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#create-review-panel" href="#create-review-form"> Write a Review </a>
+                </h4>
+            </div>
+            <div id="create-review-form" class="panel-collapse collapse">
+                <div class="panel-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<hr />
