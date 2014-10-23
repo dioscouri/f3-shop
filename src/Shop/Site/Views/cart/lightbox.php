a573c93ce72fcf23b07e1c821ff69d33e6d5e50b
Chris French <chris@ammonitenetworks.com>
2014-10-23 10:27:23 -0600

    Ading Quantity to the lightbox
    request from amritasingh, but it seems  to apply to everyone so adding to core instead of view override.

--- src/Shop/Site/Views/cart/lightbox.php
+++ src/Shop/Site/Views/cart/lightbox.php
@@ -3,10 +3,11 @@
     <h2>Your cart is empty!</h2>
 <?php } ?>
 
-<?php if (!empty($cart->items)) { $count = count($cart->items); ?>
+<?php if (!empty($cart->items)) { $count = count($cart->items); $qty= 0; ?>
 <div class="cart-lightbox-container">
     <div class="cart-lightbox-body">
         <?php foreach ($cart->items as $key=>$item) { ?>
+        <?php $qty = $qty + \Dsc\ArrayHelper::get($item, 'quantity');?>
         <div class="cart-item">
             <div class="row">        
                 <div class="col-xs-4 col-md-2">
@@ -37,8 +38,13 @@
         <div class="row">
             <div class="col-xs-6 col-md-6">
                 <h4>
-                Items: <?php echo $count; ?>
-                </h4>                
+                <span class="cart-items">
+                            Items: <?php echo $count; ?>
+                </span>
+    
+                <span class="cart-quantity"> Qty:  <?php echo $qty; ?></span>
+                </h4>   
+                              
             </div>
             <div class="col-xs-6 col-md-6">
                 <h4 class="pull-right">
