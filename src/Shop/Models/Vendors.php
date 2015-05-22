f4a779dcf02a78f397c0c9c806a65c7a23eb3a81
RallySport DMZ <chris.french@rallysportdirect.com>
2015-05-22 16:16:20 -0600

    adding vendors file

---
+++ src/Shop/Models/Vendors.php
@@ -0,0 +1,36 @@
+<?php 
+namespace Shop\Models;
+
+class Vendors extends \Dsc\Mongo\Collections\Describable
+{	
+	
+    protected $__collection_name = 'shop.vendors';
+    protected $__type = 'shop.vendors';
+    protected $__config = array(
+        'default_sort' => array(
+            'title' => 1
+        ),
+    );
+    
+    
+    protected function fetchConditions() {
+    	
+    	parent::fetchConditions();
+    }
+    
+    protected function beforeValidate()
+    {
+    	parent::beforeValidate();
+    }
+    
+    protected function beforeUpdate()
+    {
+        
+        return parent::beforeUpdate();
+    }
+    
+    protected function afterUpdate()
+    {
+        
+    }
+}
